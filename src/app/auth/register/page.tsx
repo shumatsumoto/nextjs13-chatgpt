e0ab7a0773d569e066ecca5979005019385426b0
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-12-18 12:19:41 +0900

    バリデーション（React-hook-form）

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,15 +1,43 @@
+"use client";
+
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <form className="bg-white p-8 rounded-lg shadow-md w-96">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white p-8 rounded-lg shadow-md w-96"
+      >
         <h1 className="mb-4 text-2xl text-gray-700 font-medium">新規登録</h1>
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-600">
             Email
           </label>
-          <input type="text" className="mt-1 border-2 rounded-md w-full p-2" />
+          <input
+            {...register("email", { required: "メールアドレスは必須です" })}
+            type="text"
+            className="mt-1 border-2 rounded-md w-full p-2"
+          />
+          {errors.email && (
+            <span className="text-red-600 text-sm">{errors.email.message}</span>
+          )}
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-600">
@@ -24,7 +52,7 @@ const Register = () => {
         <div className="flex justify-end">
           <button
             type="submit"
-            className="bg-pink-300 text-white font-bold py-2 px-4 rounded hover:bg-pink-400"
+            className="bg-pink-300 text-white font-bold py-2 px-4 rounded hover:bg-pink-400 mt-4"
           >
             新規登録
           </button>
