37510be77512b47f1cf87d685c7509ba657695a0
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-12-18 15:32:08 +0900

    Chatコンポーネント作成

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <div className="bg-gray-200 h-full p-4 flex flex-col">
+      <h2 className="text-2xl font-semibold mb-4">Room1</h2>
+      <div className="flex-grow overflow-y-auto mb-4">
+        <div className="text-right">
+          <div className="bg-pink-400 inline-block rounded px-4 py-2 mb-2">
+            <p className="text-white font-medium">Hello</p>
+          </div>
+        </div>
+        <div className="text-left">
+          <div className="bg-green-400 inline-block rounded px-4 py-2 mb-2">
+            <p className="text-white font-medium">How are you?</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex-shrink-0 relative">
+        <input
+          type="text"
+          placeholder="Send a Message"
+          className="border-2 rounded w-full pr-10 focus:outline-none p-2"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
