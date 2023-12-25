34d83a68474f4aad1c385edc6711a7d9a252b5d4
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-12-25 13:53:04 +0900

    入力値をfirestoreに保存

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { FaPaperPlane } from "react-icons/fa6";
+import { db } from "../../../firebase";
+import { addDoc, collection, doc, serverTimestamp } from "firebase/firestore";
 
 const Chat = () => {
+  const [inputMessage, setInputMessage] = useState<string>("");
+  const sendMessage = async () => {
+    if (!inputMessage.trim()) return;
+    const messageData = {
+      text: inputMessage,
+      sender: "user",
+      createdAt: serverTimestamp(),
+    };
+
+    // メッセージをFirestoreに保存
+    const roomDocRef = doc(db, "rooms", "");
+    const messageCollectionRef = collection(roomDocRef, "messages");
+    await addDoc(messageCollectionRef, messageData);
+  };
+
   return (
     <div className="bg-gray-200 h-full p-4 flex flex-col">
       <h2 className="text-2xl font-semibold mb-4">Room1</h2>
@@ -23,8 +42,12 @@ const Chat = () => {
           type="text"
           placeholder="Send a Message"
           className="border-2 rounded w-full pr-10 focus:outline-none p-2"
+          onChange={(e) => setInputMessage(e.target.value)}
         />
-        <button className="absolute right-4 flex items-center inset-y-0">
+        <button
+          className="absolute right-4 flex items-center inset-y-0"
+          onClick={() => sendMessage()}
+        >
           <FaPaperPlane />
         </button>
       </div>
